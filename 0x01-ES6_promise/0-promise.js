2789911e74eea70b3ff715b769b0612f43bc6e04
hertheyhermee <tiamiyuhayzed@gmail.com>
2024-10-03 17:31:29 +0100

    update on 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,15 +2,15 @@
 export default function getResponseFromAPI() {
     // Return a new promise
     return new Promise((resolve, reject) => {
-      // Do some asynchronous work
-      setTimeout(() => {
-        try {
-          // Fulfill the promise with a value
-          resolve('Response from API');
-        } catch (error) {
-          // Reject the promise with a reason
-          reject(error);
-        }
-      }, 1000);
+        // Do some asynchronous work
+        setTimeout(() => {
+            try {
+                // Fulfill the promise with a value
+                resolve('Response from API');
+            } catch (error) {
+                // Reject the promise with a reason
+                reject(error);
+            }
+        }, 1000);
     });
 }
