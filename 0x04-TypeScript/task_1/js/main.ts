226cb0fa48f869abe97107f17e79125d372d73dc
hertheyhermee <tiamiyuhayzed@gmail.com>
2024-10-10 01:08:58 +0100

    task_!

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,32 @@
+// Define the Teacher interface
+interface Teacher {
+    readonly firstName: string;  // Readonly makes it modifiable only on initialization
+    readonly lastName: string;   // Readonly ensures it can't be modified later
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;  // Optional attribute
+    location: string;
+    [key: string]: any;          // This allows adding any additional properties dynamically
+}
+
+// Create a Teacher class implementing the interface
+class TeacherClass implements Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    location: string;
+    [key: string]: any;
+
+    constructor(firstName: string, lastName: string, fullTimeEmployee: boolean, location: string, additionalAttributes: any = {}) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullTimeEmployee = fullTimeEmployee;
+        this.location = location;
+
+        // Assign any additional attributes to the teacher object
+        Object.assign(this, additionalAttributes);
+    }
+}
+
+// Example usage
+const newTeacher = new TeacherClass('John', 'Doe', true, 'New York', { contract: true, yearsOfExperience: 5 });
+console.log(newTeacher);
